9b2c153a0416f94523b61c938684d3383c586b7a
eunjaelim <imeunjae361@gmail.com>
2024-08-08 17:44:27 +0900

    polymolphism

---
+++ java-basic/src/extends1/access/basic/Child.java
@@ -0,0 +1,7 @@
+package extends1.access.basic;
+
+public class Child extends Parent{
+    public void childMethod(){
+        System.out.println("Child.childMethod");
+    }
+}
---
+++ java-basic/src/extends1/access/basic/Parent.java
@@ -0,0 +1,7 @@
+package extends1.access.basic;
+
+public class Parent {
+    public void parentMethod(){
+        System.out.println("Parent.parentMethod");
+    }
+}
